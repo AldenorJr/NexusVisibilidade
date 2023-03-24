b8137a117a16aef0bd06308f52fd62d728dd0e87
YTNexus <aldenorjr500@yahoo.com.br>
2023-03-24 13:43:24 -0300

    Primeiro commit

---
+++ src/main/java/br/com/nexus/visibilidade/plugin/cache/Cache.java
@@ -0,0 +1,11 @@
+package br.com.nexus.visibilidade.plugin.cache;
+
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+
+public class Cache {
+
+    public static ArrayList<Player> listPlayerOn = new ArrayList<>();
+
+}
